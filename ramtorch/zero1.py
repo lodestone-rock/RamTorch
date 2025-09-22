53a491d56b88fb25e145cf341df5d49a2cb5fe83
lodestone-rock <lodestone.rock@gmail.com>
2025-09-22 18:09:14 +0700

    add docs for zero1

--- ramtorch/zero1.py
+++ ramtorch/zero1.py
@@ -3,17 +3,46 @@ import torch.distributed as dist
 
 def create_zero_param_groups(param_groups, world_size):
     """
-    Create parameter groups for ZeRO-1 optimizer sharding and generate a map
-    of parameter owners for broadcasting.
+        Create parameter groups for ZeRO-1 optimizer sharding and generate a map
+        of parameter owners for broadcasting.
 
-    Args:
-        param_groups: A list of parameter groups in the standard PyTorch format.
-            Example: [{'params': model.parameters(), 'lr': 0.001}]
-        world_size: Total number of ranks.
+        Args:
+            param_groups: A list of parameter groups in the standard PyTorch format.
+                Example:
+                ```python
+                [{'params': model.parameters(), 'lr': 0.001}]
+                ```
+            world_size: Total number of ranks.
 
-    Returns:
-        dict: similar to param_groups but chunked into ranks
-    """
+        Returns:
+            dict: similar to param_groups but chunked into ranks
+
+        Example of input format:
+        ```python
+        param_groups = [
+            {'params': norm_params, 'lr': 1e-3, 'weight_decay': 0.0},
+            {'params': other_params, 'lr': 1e-3, 'weight_decay': 0.01}
+        ]
+        ```
+
+        Example of output format, world_size=2:
+        ```python
+        # technically you can just construct this format
+        # by yourself with your own sharding method
+        rank_param_groups = {
+            0: [
+                {'params': norm_params_shard_0, 'lr': 1e-3, 'weight_decay': 0.0},
+                {'params': other_params_shard_0, 'lr': 1e-3, 'weight_decay': 0.01}
+            ],
+            1: [
+                {'params': norm_params_shard_1, 'lr': 1e-3, 'weight_decay': 0.0},
+                {'params': other_params_shard_1, 'lr': 1e-3, 'weight_decay': 0.01}
+            ],
+        }
+        # optimize the shard 
+        optim = AdamW(rank_param_groups[rank])
+        ```
+        """
     rank_param_groups = {rank: [] for rank in range(world_size)}
     
     for group_idx, group in enumerate(param_groups):
