4808ec19d75345a99167970a880ed5960fe87439
lodestone-rock <lodestone.rock@gmail.com>
2026-08-06 02:24:54 -0700

    Bump to v1.2.0
    Adds tuple/nested-tuple pipeline inputs, symmetric Pipeline.infer (output
    mirrors input shape), dedicated pipeline docs (docs/pipeline_parallel.md), and
    the frozen-encoder overlap example.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ramtorch",
-    version="1.1.0",
+    version="1.2.0",
     author="Lodestone",
     author_email="lodestone.rock@gmail.com",
     description="RAM is All You Need",
